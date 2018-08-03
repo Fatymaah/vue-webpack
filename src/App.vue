2407f57542e54c4005174942c7603fb9d69b6286
Fatymaah <fatymahmohamed@gmail.com>
2018-08-03 12:52:51 +0300

    weatherApp

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,6 @@
 <template>
   <div class="container">
-    <h3>{{ title }}</h3>
-    <form>
-    <textarea placeholder="Add new Task" v-model="newTask"></textarea>
-    <p>{{newTask}}</p>
-    <button type="button" v-on:click="addTask" > Add Task {{tasks.length}} </button>
-    </form>
-
-    <ul>
-    <li v-for="tasks in tasks"> {{tasks}} 
-
-    </li>
-    </ul>
-    
+    <input type="search" id="city-search" placeholder="Search City">
 
 
    
